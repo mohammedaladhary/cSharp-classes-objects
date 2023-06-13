fb5f4fdb9b0e78c3933fc0ab88a5e58efd754218
mohammedaladhary <caladhary@gmail.com>
2023-06-13 15:22:56 +0400

    classes

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    class Car
+    {
+        public string color = "red";
+        public int speed = 120;
+        public int model = 2023;
+    }
+    private static void Main(string[] args)
+    {
+        Car myObj = new Car();
+        Console.WriteLine("my car colour is: "+myObj.color);
+        Console.WriteLine("I am driving: "+myObj.speed);
+        Console.WriteLine("My car model is: "+myObj.model);
+    }
+} 
